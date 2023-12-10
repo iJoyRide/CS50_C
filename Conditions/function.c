0b9fedb9ab7b04a80a1989b335c7a975e675cb7e
iJoyRide <sumfeis3@gmail.com>
2023-12-10 21:56:04 +0800

    function

---
+++ Conditions/function.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+
+int get_int(void){
+    int x;
+    printf("enter a value: \n");
+    scanf("%d", &x);
+    return x;
+}
+
+
+int get_size(void){
+
+    int n;
+    do
+    {
+        n = get_int();
+    } while (n < 1);
+    return n;
+    
+}
+
+void print_grid(int size){
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
+
+int main (void){
+    // Get size of grid
+    int n = get_size();
+
+    //Print grid of bricks
+    print_grid(n);
+
+}
+
